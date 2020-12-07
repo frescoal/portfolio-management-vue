acf8341294349f36ba1b5410d14728553050258a
Alain Fresco <alain.fresco@gmail.com>
2020-12-07 21:25:33 +0100

    :wrench: Setting up devise auth Token

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+
+      # Dashboard
+      get 'key_numbers', to: 'dashboard#key_numbers'
+
+      # Resources
+      resources :meta_accounts, only: [:index]
     end
   end
 end
